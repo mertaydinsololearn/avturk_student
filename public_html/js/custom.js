5ef66e3a3ba69569f4a1cf55bf84f5918cdbd6cb
Mert <mertaydin99@hotmail.com>
2024-03-14 15:22:43 +0300

    Last fix was implemented wrong.

--- public_html/js/custom.js
+++ public_html/js/custom.js
@@ -160,20 +160,12 @@ signInputs.forEach(function(element) {
 
 
 //As when a password or email doesn't match or can't be found one of them is called with setCustomValidity & reportValidity the other gets affected
-document.getElementById("sign_in_email").addEventListener("input", (e) => {
-    document.getElementById("sign_in_password").setCustomValidity('');
-    document.getElementById("sign_in_password").reportValidity();
-});
+
 document.getElementById("sign_in_password").addEventListener("input", (e) => {
     document.getElementById("sign_in_email").setCustomValidity('');
     document.getElementById("sign_in_email").reportValidity();
 });
 
-
-document.getElementById("sign_up_email").addEventListener("input", (e) => {
-    document.getElementById("sign_up_password").setCustomValidity('');
-    document.getElementById("sign_up_password").reportValidity();
-});
 document.getElementById("sign_up_password").addEventListener("input", (e) => {
     document.getElementById("sign_up_email").setCustomValidity('');
     document.getElementById("sign_up_email").reportValidity();
